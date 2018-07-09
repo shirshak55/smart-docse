2ecc1edc9b49c199f2cd49fba4b60ab01c2b0543
Shirshak Bajgain <bloggervista@gmail.com>
2018-07-09 22:42:08 +0545

    Clean

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -10,32 +10,17 @@
   }
 </script>
 
-<style scss>
-
-// Tabler bootstrap variables
-@import "../../vendor/tabler/scss/variables";
-
-// Custom Variables
-@import "variables";
-
-// Thème CoreUI
-@import "~@coreui/coreui/scss/coreui";
-@import "~bootstrap-vue/dist/bootstrap-vue.css";
-
-// Tabler components
-@import "../../vendor/tabler/scss/dashboard/functions";
-@import "../../vendor/tabler/scss/dashboard/cards";
-@import "../../vendor/tabler/scss/dashboard/tag";
-@import "../../vendor/tabler/scss/dashboard/forms/custom-switch";
-@import "../../vendor/tabler/scss/dashboard/fonts/feather";
-
-// Plugins
-@import "~noty/src/noty";
-@import "~noty/src/themes/bootstrap-v4";
-@import "~sweetalert2/src/sweetalert2";
-@import "~flatpickr/dist/flatpickr.css";
-@import "../buttons";
-
+<style lang="scss">
+@import "./assets/tabler/scss/variables";
+@import "./assets/tabler/scss/dashboard/functions";
+@import "./assets/tabler/scss/dashboard/cards";
+@import "./assets/tabler/scss/dashboard/tag";
+@import "./assets/tabler/scss/dashboard/forms/custom-switch";
+@import "./assets/tabler/scss/dashboard/fonts/feather";
+@import "noty/src/noty";
+@import "noty/src/themes/bootstrap-v4";
+@import "sweetalert2/src/sweetalert2";
+@import "flatpickr/dist/flatpickr.css";
 [v-cloak] {
   display: none;
 }
