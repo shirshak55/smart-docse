1a07367c8647d71523051041809e4eca28bc7619
Shirshak Bajgain <bloggervista@gmail.com>
2018-07-09 22:09:27 +0545

    Fixed versions

--- src/renderer/assets/tabler/plugins/input-mask/plugin.js
+++ src/renderer/assets/tabler/plugins/input-mask/plugin.js
@@ -1,8 +1,8 @@
 require.config({
-    shim: {
-        'input-mask': ['jquery', 'core']
-    },
-    paths: {
-        'input-mask': 'assets/plugins/input-mask/js/jquery.mask.min'
-    }
-});
+  shim: {
+    'input-mask': ['jquery', 'core']
+  },
+  paths: {
+    'input-mask': 'assets/plugins/input-mask/js/jquery.mask.min'
+  }
+})
--- src/renderer/assets/tabler/plugins/prismjs/plugin.js
+++ src/renderer/assets/tabler/plugins/prismjs/plugin.js
@@ -1,18 +1,18 @@
 require.config({
-    paths: {
-        'prismjs': 'assets/plugins/prismjs/js/prism.pack',
-    },
-	shim: {
-		prism: {
-			exports: "Prism"
-		}
-	}
-});
+  paths: {
+    'prismjs': 'assets/plugins/prismjs/js/prism.pack'
+  },
+  shim: {
+    prism: {
+      exports: 'Prism'
+    }
+  }
+})
 
-require(['prismjs', 'jquery'], function(prismjs, $){
-    $(document).ready(function(){
-        // $('[class^="language-"]').each(function(i, block) {
+require(['prismjs', 'jquery'], function (prismjs, $) {
+  $(document).ready(function () {
+    // $('[class^="language-"]').each(function(i, block) {
 	     //    Prism.highlightElement(block);
-        // });
-    });
-});
+    // });
+  })
+})
