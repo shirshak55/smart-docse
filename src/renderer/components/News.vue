326f4539c4b5236168eb017b70463835a0da742b
Shirshak Bajgain <bloggervista@gmail.com>
2018-07-10 15:59:54 +0545

    Fixes

--- src/renderer/components/News.vue
+++ src/renderer/components/News.vue
@@ -1,6 +1,36 @@
 <template>
-	<div>
-		News
+	<div class='row'>
+		<div class="col-md-3">
+			<div class="list-group list-group-transparent mb-0">
+				<router-link to="/news/all" class="list-group-item list-group-item-action d-flex align-items-center" active-class='active'>
+					<span class="icon mr-3"><i class="fe fe-inbox"></i></span>
+					All News
+				</router-link>
+				<router-link to="/news/ku" class="list-group-item list-group-item-action d-flex align-items-center" active-class='active'>
+					<span class="icon mr-3"><i class="fe fe-inbox"></i></span>
+					KU Related News
+				</router-link>
+				<router-link to="/news/ku" class="list-group-item list-group-item-action d-flex align-items-center" active-class='active'>
+					<span class="icon mr-3"><i class="fe fe-inbox"></i></span>
+					DOCSE Related News
+				</router-link>
+				<router-link to="/news/ku" class="list-group-item list-group-item-action d-flex align-items-center" active-class='active'>
+					<span class="icon mr-3"><i class="fe fe-inbox"></i></span>
+					Other News
+				</router-link>
+			</div>
+		</div>
+		
+		<div class="col-md-9">
+			<div class="card">
+				<div class="card-header">
+					<h3 class='card-title'>KU closing today</h3>
+				</div>
+				<div class="card-body">
+					<p>We heard KU will be closed for saturday</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
