b521255b0eae00677c9b49897e32950be3e079c1
Shirshak Bajgain <8097377+shirshak786@users.noreply.github.com>
2018-08-02 19:05:27 +0545

    Update AboutUs.vue
    As they were just for names. They didn't do anything :)

--- src/renderer/components/AboutUs.vue
+++ src/renderer/components/AboutUs.vue
@@ -10,34 +10,6 @@
         </div>
       </div>
 
-      <div class="col-md-4">
-          <div class="card">
-            <div class="card-alert alert alert-info mb-0">Created By:</div>
-            <div class="card-body">
-              Ayush Moktan
-            </div>
-          </div>
-      </div>
-
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-alert alert alert-danger mb-0">Created By:</div>
-          <div class="card-body">
-            Atul Jha
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-4 offset-md-4">
-        <div class="card">
-          <div class="card-alert alert alert-success mb-0">Supervisor</div>
-          <div class="card-body">
-            Miss Praynita Karki
-          </div>
-        </div>
-      </div>
     </div>
 	</div>
 </template>
